00b180559902d2dac3c4462eef85981d328bfe02
NutNapatong <napatpongct@gmail.com>
2026-02-19 10:22:59 +0700

    feat: create AttendanceApproval component with initial UI for attendance and leave approval cards.

--- src/components/Attendance/AttendanceApproval/AttendanceApproval.js
+++ src/components/Attendance/AttendanceApproval/AttendanceApproval.js
@@ -1,7 +1,113 @@
+import { Card } from 'react-bootstrap';
+import { DatePicker, Select, Form, Input } from 'antd';
+import { SearchToolBtnBootstrap, ClearToolBtnBootstrap, AddToolBtnBootstrap, EditToolBtnBootstrap, DeleteToolBtn } from '../../Utilities/Buttons/Buttons';
+import { RejectTag, ApproveTag, PendingApproveTag } from "../../Utilities/StatusTag/StatusTag";
+import TokenService from '../../../services/token.service';
+import TableUI from '../../Utilities/Table/TableUI';
+import { getDropdown } from '../../../services/dropdown.service';
+import { noticeShowMessage } from '../../Utilities/Notification';
+import Loading from "../../Utilities/Loading";
 const AttendanceApproval = () => {
     return (
-        <div>
-            <h1>Attendance & Leave Approval</h1>
+        <div style={{ paddingLeft: '20px', paddingRight: '20px', paddingBottom: '40px', backgroundColor: '#e9ecef', minHeight: '80vh' }}>
+
+
+            {/* Attendance Approval Card */}
+            <Card
+                className="shadow-sm border-0"
+                style={{
+                    borderRadius: "6px",
+                    overflow: "hidden",
+                    boxShadow: "0px 4px 4px rgba(0,0,0,0.25)",
+                }}>
+                <Card.Header
+                    style={{
+                        backgroundColor: "#A0BDFF",
+                        padding: "14px 20px",
+                        fontSize: "22px",
+                        fontWeight: 600,
+                        color: "black",
+                        borderBottom: "1px solid #d9d9d9",
+                    }}
+                >
+                    Attendance Approval
+                </Card.Header>
+                <Card.Body className="p-3">
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            flexWrap: "wrap",
+                            gap: "15px",
+                            background: "white",
+                        }}
+                    >
+
+                        
+                    </div>
+
+                </Card.Body>
+            </Card>
+
+            {/* Leave Approval Card */}
+            <Card
+                className="shadow-sm border-0"
+                style={{
+                    borderRadius: "6px",
+                    overflow: "hidden",
+                    boxShadow: "0px 4px 4px rgba(0,0,0,0.25)",
+                    marginTop: "30px",
+                }}>
+                <Card.Header
+                    style={{
+                        backgroundColor: "#A0BDFF",
+                        padding: "14px 20px",
+                        fontSize: "22px",
+                        fontWeight: 600,
+                        color: "black",
+                        borderBottom: "1px solid #d9d9d9",
+                    }}
+                >
+                    Leave Approval
+                </Card.Header>
+                <Card.Body className="p-3">
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            flexWrap: "wrap",
+                            gap: "15px",
+                            background: "white",
+                        }}
+                        
+                    >
+
+                        <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                            <span style={{ fontWeight: 700, fontSize: '16px' }}>วันที่:</span>
+                            <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+                                <DatePicker
+                                    format="DD/MM/YYYY"
+                                    placeholder="DD/MM/YYYY"
+                                    inputReadOnly={true}
+                                    value={null}
+                                    onChange={null}
+                                    style={{ width: 140 }}
+                                />
+                                <span>-</span>
+                                <DatePicker
+                                    format="DD/MM/YYYY"
+                                    placeholder="DD/MM/YYYY"
+                                    inputReadOnly={true}
+                                    value={null}
+                                    onChange={null}
+                                    style={{ width: 140 }}
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                </Card.Body>
+            </Card>
         </div>
     );
 };
